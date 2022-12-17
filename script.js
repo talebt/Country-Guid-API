917ed64b822a7535637b55e599c0823329b455e1
Thabet <thabettaleb@gmail.com>
2022-12-16 19:37:28 -0600

    adding console

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let searchBtn = document.getElementById("search-btn");
 let country = document .getElementById("country");
 searchBtn.addEventListener("click", ()=>{
-    let countryName= country.value;
+    let countryName= "usa";
 
     let finalURL= `https://restcountries.com/v3.1/name/${country}?fullText=true
     `;
-    console.log(finalURL);
+    //console.log(finalURL);
+    fetch(finalURL)
+    .then((response) => response.json())
+    .then((data) =>{
+        console.log(data);
 
-})
+    }) ;
+
+});
